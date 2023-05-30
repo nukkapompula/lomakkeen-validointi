665e25dd6e7578c63a6c36c2053e0499f0797803
Arto Eskelinen <arto.eske@gmail.com>
2023-05-30 08:49:46 +0300

    ID:n tarkistus toimii

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,17 @@
-console.log("no kas päiwää waan")
+document.querySelector("button").addEventListener("click", tarkistaLomake);
+
+let virheID = document.getElementById("huonoID");
+
+let kaikkiOK = true;
+
+function tarkistaLomake(event){
+    event.preventDefault();
+    let iidee = document.getElementById("kID").value;
+    if(iidee.length<6){
+        document.getElementById("kID").value = "";
+        virheID.style.display = "block";
+        kaikkiOK = false;
+    } else {
+        virheID.style.display = "none";
+    }
+}
