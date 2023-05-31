8475a03c31b6ca27f114775780fb308ee864de96
Arto Eskelinen <arto.eske@gmail.com>
2023-05-31 09:33:42 +0300

    virhe validaatiossa korjattu

--- toiminnot.js
+++ toiminnot.js
@@ -10,79 +10,78 @@ let virheSposti = document.getElementById("huonoSposti");
 let virheSukupuoli = document.getElementById("huonoSukupuoli");
 let virheKieli = document.getElementById("huonoKieli");
 
-let kaikkiOK = true;
-
 function tarkistaLomake(event){
     event.preventDefault();
+    let kaikkiOK = 0;
     // jokainen kenttä käydään läpi, alkaen...
     // Käyttäjä-ID
     let iidee = document.getElementById("kID").value;
     if(iidee.length<6){
         virheID.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virheID.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Salasana
     let salasana = document.getElementById("salasana").value;
     if(salasana.length<1){
         virheSalasana.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virheSalasana.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Nimi
     let nimi = document.getElementById("nimi").value;
     if(nimi.length<1){
         virheNimi.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virheNimi.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Osoite
     let osoite = document.getElementById("osoite").value;
     if(osoite.length<1){
         virheOsoite.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virheOsoite.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Maa
     let maa = document.getElementsByTagName("select")[0].value;
     if(maa=="oletus"){
         virheMaa.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virheMaa.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Postinumero
     let postinumero = document.getElementById("postinro").value;
     if(postinumero.length!=5){
         virhePostinro.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     } else {
         virhePostinro.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     }
 
     // Sähköposti
     let sposti = document.getElementById("sposti").value;
     if(spostiTarkastus(sposti)){
         virheSposti.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     } else {
         virheSposti.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     }
 
     // Sukupuoli
@@ -95,10 +94,10 @@ function tarkistaLomake(event){
     }
     if(valinta>0){
         virheSukupuoli.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     } else {
         virheSukupuoli.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     }
 
     // Kieli
@@ -111,13 +110,13 @@ function tarkistaLomake(event){
     }
     if(ruksi>0){
         virheKieli.style.display = "none";
-        kaikkiOK = true;
+        kaikkiOK += 1;
     } else {
         virheKieli.style.display = "block";
-        kaikkiOK = false;
+        kaikkiOK += 0;
     }
 
-    if(kaikkiOK){
+    if(kaikkiOK==9){
         alert("Rekisteröityminen onnistui! Tervetuloa!")
     } else {
         alert("Rekisteröityminen epäonnistui. Korjaa punaisella merkityt puutteet tiedoissa.")
@@ -125,6 +124,7 @@ function tarkistaLomake(event){
 }
 
 function spostiTarkastus(osoite){
+    // osoitteen testaus ns. Regular Expressionin avulla
     const ilmaus = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return ilmaus.test(String(osoite).toLowerCase());
 }
